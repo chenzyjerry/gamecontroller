38ccddb9b0e0318efa4318257544880c745b1807
Jerry Chen <91328333+chenzyjerry@users.noreply.github.com>
2023-10-14 16:17:56 -0400

    updated commenting

--- UNO_controller.ino
+++ UNO_controller.ino
@@ -1,8 +1,8 @@
-int B1_pin = 2;
-int B2_pin = 3;
-int SW_pin = 8;
+int B1_pin = 2; //set digital pin for top button
+int B2_pin = 3; //set digital pin for bottom button
+int SW_pin = 8; //set digital pin for joystick button
 
-void setup() {
+void setup() { //set pin mode for all buttons
  pinMode(B1_pin,INPUT_PULLUP);
  pinMode(B2_pin,INPUT_PULLUP);
  pinMode(SW_pin,INPUT_PULLUP);
@@ -11,38 +11,38 @@ Serial.begin(9600);
 }
 
 void loop() {
-  int X = analogRead(A1);
+  int X = analogRead(A1); //read X and Y values for joystick axes
   int Y = analogRead(A0);
   int x = map(X, 1, 1023, 1917, 2);
   int y = map(Y, 1, 1023, 2, 1077);
   
 if(digitalRead(B1_pin)==LOW){
-Serial.print("a");
+Serial.print("a"); //prints 'a' if top button is not pressed
 
 }else{
-  Serial.print("b");
+  Serial.print("b"); //prints 'b' if top button is pressed
 }
 
 Serial.print(" ");
 if(digitalRead(B2_pin)==LOW){
-Serial.print("e");
+Serial.print("e"); //prints 'e' if top button is not pressed
 
 }else{
-  Serial.print("f");
+  Serial.print("f"); //prints 'f' if top button is pressed
 }
 
 Serial.print(" ");
 if(digitalRead(SW_pin)==LOW){
-Serial.print("c");
+Serial.print("c"); //prints 'c' if top button is not pressed
 
 }else{
-  Serial.print("z");
+  Serial.print("z"); //prints 'z' if top button is pressed
 }
 
 Serial.print(" ");
-Serial.print(x);
+Serial.print(x); //print joystick X axis value
 Serial.print(" ");
-Serial.print(y);
+Serial.print(y); //print joystick Y axis value
 Serial.print(" ");
 Serial.println("m");
 delay(180);
