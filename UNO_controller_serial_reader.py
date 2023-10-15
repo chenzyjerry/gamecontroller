b1289911a521017fbefeeae4a44473346fc5a08b
Jerry Chen <91328333+chenzyjerry@users.noreply.github.com>
2023-10-14 23:43:48 -0400

    Add files via upload

--- UNO_controller_serial_reader.py
+++ UNO_controller_serial_reader.py
@@ -1,12 +1,22 @@
 import serial
 import pyautogui as pg
+import os
+import platform
 
 
 pg.FAILSAFE=False
 
+# be lazy
+if platform.system() == 'Windows':
+    ser = serial.Serial("COM3",'9600')
+elif platform.system() == 'Darwin':
+    ser = serial.Serial("/dev/cu.usbmodem212101",'9600')
+elif platform.system() == 'Linux':
+    ser = serial.Serial("/dev/ttyACM0",'9600')
 
-ser = serial.Serial("COM3",'9600')
-while True:
+leftDown = False
+rightDown = False
+while 1:
     read = ser.readline().decode('ascii')
     readArray = read.split()
     b = readArray[0]
@@ -22,5 +32,20 @@ while True:
         pg.press('b')
     if f=='f':
         pg.press('f')
+    if int(x) < 900:
+        pg.keyDown('left')
+        leftDown = True
+    elif leftDown:
+        pg.keyUp('left')
+        leftDown = False
+    if int(x) > 1000:
+        pg.keyDown('right')
+        rightDown = True
+    elif rightDown:
+        pg.keyUp('right')
+        rightDown = False
 
-    pg.moveTo(int(x), int(y))
+    if int(y) < 400:
+        pg.press('up')
+
+    # pg.moveTo(int(x), int(y))
